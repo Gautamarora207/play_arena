27788212391fd29da960f88e8349c6a2731725ae
GautamArora <gautamarora207@gmail.com>
2021-02-17 19:56:43 +0530

    animations added

--- lib/ListPage/ListPage.dart
+++ lib/ListPage/ListPage.dart
@@ -3,9 +3,35 @@ import 'package:flutter_assignment/ListPage/ListItem.dart';
 import 'package:flutter_assignment/ListPage/ErrorWidget.dart';
 import 'package:flutter_assignment/constants/constants.dart';
 
-class ListPage extends StatelessWidget {
+class ListPage extends StatefulWidget {
   final List playArenas;
   ListPage(this.playArenas);
+
+  @override
+  _ListPageState createState() => _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> with TickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<Offset> _animation;
+
+  void initState() {
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    )..forward();
+
+    _animation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: const Offset(0, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInToLinear,
+    ));
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +42,18 @@ class ListPage extends StatelessWidget {
         ),
         backgroundColor: black,
       ),
-      body: playArenas == null
+      body: widget.playArenas == null
           ? buildError(context)
           : Container(
               color: lightGrey,
-              child: ListView.builder(
-                itemBuilder: (BuildContext context, i) {
-                  return ListItem(playArenas[i]);
+              child: AnimatedList(
+                initialItemCount: widget.playArenas.length,
+                itemBuilder: (BuildContext context, i, animation) {
+                  return SlideTransition(
+                    child: ListItem(widget.playArenas[i]),
+                    position: _animation,
+                  );
                 },
-                itemCount: playArenas.length,
               ),
             ),
     );
